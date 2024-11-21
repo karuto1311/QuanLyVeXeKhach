afe1734e49054a675e549a74258a1d8418517ff4
ThienHiTech <thienhitech@gmail.com>
2024-11-21 10:18:45 +0700

    Tra cứu vé

--- frontend/WebXeKhach/src/Components/TicketContent.jsx
+++ frontend/WebXeKhach/src/Components/TicketContent.jsx
@@ -4,8 +4,11 @@ import '../assets/Css/Ticket.css';
 
 function TicketContent(){
     return(
+        
         <div>
             <h1>TRA CỨU THÔNG TIN ĐẶT VÉ</h1>
+        <div className="ticket-container">
+           <div className="ticket-box">
             <div>Tìm thông tin vé của mình</div>
             <div className="form-ticket">
                 <form className="form-ticket">
@@ -18,10 +21,12 @@ function TicketContent(){
                         <input type="text"></input>
                     </div>
                     <div>
-                        <button className="button-ticket">Tìm vé</button>
+                        <button className="button-ticket-2">Tìm vé</button>
                     </div>
                 </form>
             </div>
+            </div>
+        </div>
         </div>
     )
 
